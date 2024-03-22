fd0d4eda38e4f7d112c5e6391bad05dbfee58166
Andrzzej <s28492@pjwstk.edu.pl>
2024-03-22 07:41:59 +0100

    Added part of game logic

--- Uno/Card.py
+++ Uno/Card.py
@@ -35,18 +35,30 @@ class Player:
     def draw(self, card):
         self.hand.append(card)
 
-    def play_card(self):
+    def play_move(self, top_card_on_pile):
+        print(f"Card on the top of the pile: {top_card_on_pile}")
         print(self.show_hand())
-        print("Write whitch card you wanna play:")
+        print("Write what you wanna play: [Value Color], [Draw], [Surrender]")
         value = input()
+
+        if value == "Draw":
+            value = "-1 Draw"
+        elif value == "Surrender":
+            value = "-1 Surrender"
         card = value.split(" ")
-        card = Card(int(card[0]), card[1])
-        if card in self.hand:
-            self.hand.pop(self.hand.index(card))
-            print(f"Wyłożono: | {card} |")
+        try:
+            card = Card(int(card[0]), card[1])
+        except:
+            print("You gave me wrong values you sneaky bastard!")
+            return self.play_move(top_card_on_pile)
+        if card.value == "Draw" or card.value == "Surrender":
+            return card
+        elif card in self.hand:
+            self.hand.remove(card)
+            return card
         else:
             print("It seems that you don't have this card on hand. Let's try again:")
-            self.play_card()
+            return self.play_move(top_card_on_pile)
         # return self.hand.pop()
 
 
@@ -57,11 +69,11 @@ class Game:
     def __init__(self, *args: Player):
         self.deck = self.create_deck()
         self.shuffle_deck()
-        if 1 < len(args) <= 10:             # Warunek ilości graczy
-            self.players = args
+        if 1 < len(args) <= 10:  # Warunek ilości graczy
+            self.players = [player for player in args]
         else:
             raise ValueError("Sorry, the number of players is incorrect.")
-        self.pile = [].append(self.deck.pop(0))
+        self.pile = self.deck.pop()
         self.deal_cards()
         self.direction = 1  # Kierunek gry
 
@@ -78,35 +90,65 @@ class Game:
 
     def deal_cards(self):
         for player in self.players:
-            for i in range(7):
+            for i in range(7):  # Karty startowe
                 player.draw(self.deck.pop(0))
+
+    def draw_card(self, player: Player):
+        if len(self.deck) > 0:
+
+            player.draw(self.deck.pop(0))
+        else:
+            print("Deck is empty, you have to go on without one.")
 
     def play(self):
         winner = None
-        while(winner == None):
-            for player in self.players:
-                #print(player.show_hand())
-                player.play_card()
-                if len(player.hand) == 0:
-                    winner = player
-                    break
-        print(f"Gratulacje {winner}")
-
+        player_index = 0
+        players_amount = len(self.players)
+        while winner is None:
+            player = self.players[player_index % players_amount]
+            move = player.play_move(self.pile)
+            if move.value == "Draw":
+                self.draw_card(player)
+            elif move.value == "Surrender":
+                self.players.remove(player)
+                break
+            else:
+                while move.value != self.pile.value and move.color != self.pile.color:
+                    print("You can't put this card here")
+                    move = player.play_move(self.pile)
+                self.pile = move
+                print(f"Card played: | {move} |")
+            if len(player.hand) == 0:
+                winner = player
+            if len(self.players) == 1:
+                winner = self.players[0]
+            player_index += self.direction
+        print(f"Congratulations {winner}. You won the game!!!")
 
 
 class ReverseCard(Card):
     # Specjalne zachowanie dla karty Reverse
+    def __init__(self, color, value):
+        super().__init__(color, value)
+
     pass
 
 
 class StopCard(Card):
-    pass
+    def __init__(self, color, value):
+        super().__init__(color, value)
 
 
 class Plus2Card(Card):
-    pass
+    def __init__(self, color, value):
+        super().__init__(color, value)
 
 
 class Plus4Card(Card):
-    TAKE4 = 4
-    pass
+    def __init__(self, color, value):
+        super().__init__(color, value)
+
+
+class ColorCard(Card):
+    def __init__(self, color, value):
+        super().__init__(color, value)

392958320316a7bf4c057dd2a1021af95f0009b2
Andrzzej <s28492@pjwstk.edu.pl>
2024-05-19 11:40:52 +0200

    Final version

--- Uno/main.py
+++ Uno/main.py
@@ -36,26 +36,20 @@ def main() -> None:
 
     console.print("[bold magenta]Welcome to UNO game![/bold magenta]")
     console.print("1. Play with other humans.")
-    console.print("2. Play with bots.")
-    console.print("3. Watch bots playing with each other.")
-    console.print("4. Play with humans and bots.")
+    console.print("2. Watch bots playing with each other.")
+    console.print("3. Play with humans and bots.")
 
-    choice = console.input("[bold green]Pick an option (1-4): [/bold green]")
+    choice = console.input("[bold green]Pick an option (1-3): [/bold green]")
 
     if choice == "1":
         player_names = console.input("Input players names separated by coma: ").split(',')
         players = [Player(name.strip()) for name in player_names]
         game = create_game_with_players(*players)
     elif choice == "2":
-        player_name = console.input("Input your name: ")
-        num_bots = int(console.input("How many bots you wanna add to a game? "))
-        players = [Player(player_name)] + [Bot(f"Bot{i + 1}") for i in range(num_bots)]
-        game = create_game_with_players(*players)
-    elif choice == "3":
         num_bots = int(console.input("How many bots should play? "))
         bots = [Bot(bot_names.pop()) for _ in range(num_bots if num_bots < 11 else 10)]
         game = create_game_with_players(*bots)
-    elif choice == "4":
+    elif choice == "3":
         player_names = console.input("Input players names separated by coma: ").split(',')
         num_bots = int(console.input("How many bots should play? "))
         players = [Player(name.strip()) for name in player_names]

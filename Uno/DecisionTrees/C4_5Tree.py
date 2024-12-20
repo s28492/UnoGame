a561b4399f104b1156f242825f5abb998fa19c35
s28492 <s28492@pjwstk.edu.pl>
2024-12-20 13:18:00 +0100

    -Corrected issues with calculating information gain - Added "Calculate gain_ratio"

--- Uno/DecisionTrees/C4_5Tree.py
+++ Uno/DecisionTrees/C4_5Tree.py
@@ -8,7 +8,7 @@ class C4_5Tree:
     def __init__(
             self, X_data: np.array, Y_data: np.array, remaining_data_indices: np.array = None,
             remaining_column_indices: np.array = None, labels_encodes: dict = None, parent: C4_5Tree = None,
-            node_depth: int = None
+            node_depth: int = None, is_leaf: bool = False
                  ):
         self.X_data = X_data
         self.Y_data = Y_data
@@ -23,23 +23,9 @@ class C4_5Tree:
             self.remaining_column_indices:np.array = remaining_column_indices
         self.labels_encodes = labels_encodes
         self.parent = parent
+        self.children = []
+        self.is_leaf = is_leaf
         self.node_depth = node_depth
-
-    # def calculate_column_entropy(self, column_index: int) -> float:
-    #     # Entropy(S) = - ( Sum(P * log_2 (P))
-    #     entropy = 0.0
-    #     X_column = self.X_data[self.remaining_data_indices, column_index]
-    #     unique_X_data = np.unique(X_column, return_counts=True)
-    #     # Iterate through each value in X column
-    #     for i, uniqie_X in enumerate(unique_X_data):
-    #         # Create list of indexes containing given value
-    #         indexes = np.where(X_column == uniqie_X[i])[0]
-    #         # Iterate through each value in Y column
-    #         for _, counted in np.unique(self.Y_data[indexes], return_counts=True):
-    #             p = counted[i] / indexes.shape[0]
-    #             partial_entropy = (p * np.log2(p))
-    #             entropy -= partial_entropy
-    #     return entropy
 
     def calculate_entropy(self, indexes):
         # Entropy(S) = - ( Sum(P * log_2 (P))
@@ -51,41 +37,123 @@ class C4_5Tree:
             entropy -= p * np.log2(p)
         return entropy
 
-    def calculate_information_gain_for_column(self, column_index: int, current_entropy) -> float:
+    def calculate_information_gain_for_column(self, column_index: int, current_entropy) -> tuple:
         # Column values for the current subset
         x_column = self.X_data[:, column_index]
         x_column_values, counts = np.unique(x_column[self.remaining_column_indices], return_counts=True)
+        split_indexes = []
+        values_from_splitted_indexes = []
 
         partial_entropy = 0
         for x_column_value, count in zip(x_column_values, counts):
-            proportion_of_given_value_in_column = count / self.remaining_column_indices.shape[0]
+            proportion_of_given_value_in_column = count / self.remaining_data_indices.shape[0]
 
             # Mapping filtered indexes values to remaining_column_indices for data alignment
             indexes_of_given_value = self.remaining_column_indices[
-                np.where(x_column[self.remaining_column_indices] == x_column_value)[0]
+                np.where(x_column[self.remaining_data_indices] == x_column_value)[0]
             ]
+            values_from_splitted_indexes.append(x_column_value)
+            split_indexes.append(indexes_of_given_value)
             # Calculating partial entropy
             partial_entropy += proportion_of_given_value_in_column*self.calculate_entropy(indexes_of_given_value)
 
-        return current_entropy - partial_entropy
+        return current_entropy - partial_entropy, values_from_splitted_indexes ,split_indexes
 
-    def calculate_best_information_gain(self) -> tuple:
-        information_gains = []
+    def calculate_split_information_for_column(self, column_index: int):
+        split_information = 0
+        x_column = self.X_data[:, column_index]
+        _, counts = np.unique(x_column[self.remaining_column_indices], return_counts=True)
+        for count in counts:
+            proportion_of_value = count / self.remaining_data_indices.shape[0]
+            split_information -= proportion_of_value * np.log2(proportion_of_value)
+        return split_information
+
+    def calculate_best_gain_ratio(self) -> tuple:
+        gain_ratios = []
+        all_split_values = []  # Collects split values for each column
+        all_split_indexes = []  # Collects indexes for each split
         # Current entropy for the entire dataset
         current_entropy = self.calculate_entropy(self.remaining_column_indices)
-        # Adds all columns information gains to array
+
+        # Adds all columns' information gains to an array
         for column_index in self.remaining_column_indices:
-            information_gain = self.calculate_information_gain_for_column(column_index, current_entropy)
-            information_gains.append(information_gain)
+            information_gain, split_values, split_indexes = self.calculate_information_gain_for_column(column_index,
+                                                                                                       current_entropy)
+            split_information = self.calculate_split_information_for_column(column_index)
+            gain_ratio = information_gain / split_information
 
-        information_gains = np.array(information_gains)
+            gain_ratios.append(gain_ratio)
+            all_split_values.append(split_values)
+            all_split_indexes.append(split_indexes)
 
-        best_column_index = self.remaining_column_indices[np.argmax(information_gains)]
-        max_information_gain = information_gains[best_column_index]
-        return best_column_index, max_information_gain
+        # Convert information gains to numpy array for efficient operations
+        gain_ratios = np.array(gain_ratios)
+
+        # Find the best column
+        best_column_idx = np.argmax(gain_ratios)
+        # Adjust best index to reference "remaining_column_indexes" not "indormation_gains"
+        best_column_index = self.remaining_column_indices[best_column_idx]
+        best_split_values = np.array(all_split_values[best_column_idx])
+        best_split_indexes = np.array(all_split_indexes[best_column_idx])
+
+        max_gain_ratio = gain_ratios[best_column_idx]
+        return best_column_index, max_gain_ratio, best_split_values, best_split_indexes
 
 
 
 
     def build_tree(self, max_depth: int, min_values_per_leaf: int, min_information_gain: float) -> C4_5Tree:
-        best_gain_ratio, best_attribute, renaining_data_indices = self.best_information_gain()
+        best_column_index, max_gain_ratio, best_split_values, best_split_indexes = self.calculate_best_gain_ratio()
+
+
+        if (self.node_depth == max_depth) or (max_gain_ratio < min_information_gain):
+            self.is_leaf = True
+            return self
+
+        for i in range(0, best_split_values.shape[0]):
+            is_enough_data_in_node: bool = best_split_indexes[i].shape[0] >= min_values_per_leaf
+            if is_enough_data_in_node:
+                new_node = C4_5Tree(self.X_data, self.Y_data, remaining_data_indices=best_split_indexes[i],
+                                    remaining_column_indices=best_split_indexes[i], labels_encodes=self.labels_encodes,
+                                    parent=self, node_depth=self.node_depth+1)
+                self.children.append(new_node)
+                new_node.build_tree(max_depth, min_values_per_leaf, min_information_gain)
+
+        return self
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
